d9887e9c5a57ebe80922a0d742115ad109a50faf
rightfold <rightfold@gmail.com>
2017-06-08 17:03:04 +0200

    Add AJAX interpreter for Accounts

---
+++ src/VLA/CRM/Account/AJAX.purs
@@ -0,0 +1,22 @@
+module VLA.CRM.Account.AJAX
+  ( runAccounts
+  ) where
+
+import Control.Monad.Error.Class (try)
+import Control.Monad.Eff.Exception (error)
+import Data.Argonaut.Decode.Class (class DecodeJson, decodeJson)
+import Data.Argonaut.Encode.Class (class EncodeJson, encodeJson)
+import Network.HTTP.Affjax as AJAX
+import Stuff
+import VLA.CRM.Account.Algebra (Accounts(..))
+
+runAccounts :: ∀ m a. MonadIO m => Accounts a -> m a
+runAccounts (FetchAccount accountID next) =
+  next <$> request "/CRM/Account/fetchAccount" accountID
+runAccounts (UpdateAccount accountID account next) =
+  next <$> request "/CRM/Account/updateAccount" (accountID /\ account)
+
+request :: ∀ m i o. MonadIO m => EncodeJson i => DecodeJson o => String -> i -> m (Either Error o)
+request url req = liftIO \ liftAff $ do
+  res <- try $ AJAX.post url (encodeJson req)
+  pure $ lmap error \ decodeJson \ _.response =<< res
